46a0d64f92270da14db6d544e2f6c4b5c9add7a1
idcetim <javirofe96@gmail.com>
2023-07-01 18:54:58 +0200

    Add inputs for each number in the squares

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,14 @@
+import Square from './Square'
+import '../styles/main.css'
 const Main = () => {
-
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  return (
+    <main className='main-table'>
+      {numbers.map((n: number) => (
+        <Square key={n} />
+      ))}
+    </main>
+  )
 }
 
 export default Main
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,8 +1,9 @@
+import '../styles/square.css'
 const Square = () => {
-
-  return(
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  return (
     <div className="square">
-
+      {numbers.map((n: number) =>  <input type='number' className='number-input' value={n}/>)}
     </div>
   )
 }
